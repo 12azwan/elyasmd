7fd40d3157166df83e661575fbc71c858c123b68
Alfarqun <87592560+Alfarqun@users.noreply.github.com>
2022-01-20 12:35:03 +0700

    update write.js

--- plugins/write.js
+++ plugins/write.js
@@ -11,7 +11,7 @@ let handler = async(m, { conn, usedPrefix, args, command }) => {
         if(!args[1]) throw `Contoh: ${usedPrefix + command} media plugins/tes.js`
         let saveas = await m.quoted.download()
         fs.writeFileSync(`./${args[1]}`, saveas)
-        conn.sendButton(m.chat, `Berhasil tersimpan sebagai ${args[1]}.`, watermark, 'Debounce', '.debounce')
+        conn.sendButton(m.chat, `Berhasil tersimpan sebagai ${args[1]}.`, wm, 'Debounce', '.debounce')
     } else if (args[0] === 'text') {
         if (!args[1]) throw `Masukkan nama dan tujuan file.\n${usedPrefix + command} plugins/join.js`
         require('fs').writeFileSync(`./${args[1]}`, m.quoted.text)
